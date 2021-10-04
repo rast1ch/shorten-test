0f16c6093580acc083727caa70374522fc93bb72
rast1ch <rast1ch2286@gmail.com>
2021-10-04 17:25:00 -0400

    Добавлен алгоритм с хешированием

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hashlib
 import redis
 import random
 import string
@@ -19,23 +20,30 @@ class CreationForm(Form):
     url_to = StringField('URL', [validators.Length(min=5, max=200)])
 
 
-def rand_slug():
-    """Формирования случайного url"""
-    random_slug = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
-    # Оооочень много вариантов +-1,340,095,640,625
-    if r.get(random_slug): # Это для избежания колизий
-        rand_slug()
-    else:
-        return random_slug
+def form_slug(link : str) :
+    # """Формирования случайного url"""
+    # random_slug = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+    # # Оооочень много вариантов +-1,340,095,640,625
+    # if r.get(random_slug): # Это для избежания колизий
+    #     rand_slug()
+    # else:
+    #     return random_slug
+    encoded_link = link.encode()
+    hash_object = hashlib.sha256(encoded_link)
+    return hash_object.hexdigest()[:3] + hash_object.hexdigest()[:-4:-1]
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = CreationForm(request.form)
     if request.method == 'POST' and form.validate():
-        slug = rand_slug()
-        r.set(slug, form.url_to.data)
-        r.expire(slug, 3600)
+        slug = form_slug(form.url_to.data)
+        if r.get(slug):
+            r.expire(slug, 3600) # Просто обновить счётчик
+        else:
+            r.set(slug, form.url_to.data)
+            r.expire(slug, 3600)
         return flask.redirect(f'http://127.0.0.1:5000/info/{slug}')
     return render_template('index.html', form=form)
 
